03715e35aa8c34d407e8bfee149b818054c9de4d
jmcb <joelsgp@protonmail.com>
2023-06-21 17:25:36 +0100

    Add main entry point to get_blend_version.py (not working)

--- get_blend_version.py
+++ get_blend_version.py
@@ -1,3 +1,5 @@
+import sys
+
 import bpy
 
 
@@ -8,3 +10,11 @@ def get_blend_version(blend_path: str) -> tuple[int, int, int]:
     for library in bpy.data.libraries:
         if library.filepath == blend_path:
             return library.version
+        
+
+def main():
+    print(get_blend_version(sys.argv[1]))
+
+
+if __name__ == "__main__":
+    main()

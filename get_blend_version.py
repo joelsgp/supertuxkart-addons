c0110bf5000845a30dc9b033bf048d1e5689b961
jmcb <joelsgp@protonmail.com>
2023-06-21 17:24:09 +0100

    Rework get_blend_version.py

--- get_blend_version.py
+++ get_blend_version.py
@@ -1,12 +1,10 @@
 import bpy
 
-blend_path = r"D:\asset\data\hqKAZbQSqjx\desert_bush_2.blend"
 
-with bpy.data.libraries.load(blend_path, link =  True) as (data_from, data_to):
-    pass
+def get_blend_version(blend_path: str) -> tuple[int, int, int]:
+    with bpy.data.libraries.load(blend_path, link=True) as _, _:
+        pass
 
-for library in bpy.data.libraries:
-    if library.filepath == blend_path:
-        print(library.version)
-        print(library.filepath)
-        bpy.data.libraries.remove(library)
+    for library in bpy.data.libraries:
+        if library.filepath == blend_path:
+            return library.version
